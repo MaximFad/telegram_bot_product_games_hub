b1c8e4878a4503419711d95b8faab4f332ab7c79
Maxim Fadeichev <12283182+MaximFad@users.noreply.github.com>
2026-03-21 04:17:58 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
 from telegram.ext import (
-    ApplicationBuilder, CommandHandler,
-    CallbackQueryHandler, MessageHandler,
-    filters, ConversationHandler
+    ApplicationBuilder,
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    filters,
+    ConversationHandler,
 )
 from config import TOKEN
 from handlers.start import start
 from handlers.refs import my_refs
-from handlers.menu import check_and_send
+from handlers.menu import check_and_send, materials_menu
 from handlers.admin import (
-    admin_panel, stats,
-    ask_broadcast_text, do_broadcast,
-    WAITING_BROADCAST
+    admin_panel,
+    stats,
+    ask_broadcast_text,
+    do_broadcast,
+    WAITING_BROADCAST,
 )
 
 app = ApplicationBuilder().token(TOKEN).build()
@@ -18,11 +23,13 @@ app = ApplicationBuilder().token(TOKEN).build()
 conv_handler = ConversationHandler(
     entry_points=[CallbackQueryHandler(ask_broadcast_text, pattern="^do_broadcast$")],
     states={WAITING_BROADCAST: [MessageHandler(filters.TEXT & ~filters.COMMAND, do_broadcast)]},
-    fallbacks=[]
+    fallbacks=[],
 )
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(conv_handler)
+
+app.add_handler(CallbackQueryHandler(materials_menu, pattern="^materials_menu$"))
 app.add_handler(CallbackQueryHandler(my_refs, pattern="^my_refs$"))
 app.add_handler(CallbackQueryHandler(admin_panel, pattern="^admin_panel$"))
 app.add_handler(CallbackQueryHandler(stats, pattern="^stats$"))
