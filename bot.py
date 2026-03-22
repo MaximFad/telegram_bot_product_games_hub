04357946c1a9bc5d4aa0e56d0a860c1cfb91b2e0
Maxim Fadeichev <12283182+MaximFad@users.noreply.github.com>
2026-03-23 00:58:52 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,12 +7,9 @@ from telegram.ext import (
     filters,
 )
 
-from config import TOKEN
+from config import TOKEN, DISCUSSION_GROUP_ID
 from handlers.start import start
 from handlers.refs import my_refs
-from handlers.leaderboard import show_leaderboard
-from handlers.comment_points import handle_comment_points
-from config import TOKEN, DISCUSSION_GROUP_ID
 from handlers.menu import materials_menu, check_and_send, check_subscription, show_main_menu
 from handlers.admin import (
     admin_panel,
@@ -26,6 +23,8 @@ from handlers.game_review import (
     receive_game_review,
     WAITING_GAME_REVIEW,
 )
+from handlers.leaderboard import show_leaderboard
+from handlers.comment_points import handle_comment_points
 
 
 def build_app():
@@ -56,24 +55,23 @@ def build_app():
 
     app.add_handler(CallbackQueryHandler(materials_menu, pattern="^materials_menu$"))
     app.add_handler(CallbackQueryHandler(my_refs, pattern="^my_refs$"))
-    app.add_handler(CallbackQueryHandler(show_leaderboard, pattern="^leaderboard$"))
     app.add_handler(CallbackQueryHandler(admin_panel, pattern="^admin_panel$"))
     app.add_handler(CallbackQueryHandler(stats, pattern="^stats$"))
-
     app.add_handler(CallbackQueryHandler(check_subscription, pattern="^check_subscription$"))
     app.add_handler(CallbackQueryHandler(check_and_send, pattern="^(link_|secret_)"))
     app.add_handler(CallbackQueryHandler(show_main_menu, pattern="^back_to_menu$"))
-    
+    app.add_handler(CallbackQueryHandler(show_leaderboard, pattern="^leaderboard$"))
+
     if DISCUSSION_GROUP_ID:
-    app.add_handler(
-        MessageHandler(
-            filters.Chat(chat_id=DISCUSSION_GROUP_ID)
-            & ~filters.StatusUpdate.ALL
-            & ~filters.COMMAND,
-            handle_comment_points,
+        app.add_handler(
+            MessageHandler(
+                filters.Chat(chat_id=DISCUSSION_GROUP_ID)
+                & ~filters.StatusUpdate.ALL
+                & ~filters.COMMAND,
+                handle_comment_points,
+            )
         )
-    )
-    
+
     return app
 
 
